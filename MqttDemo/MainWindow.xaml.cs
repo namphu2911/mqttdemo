7d33e67538a171120fd89794d8ff804c97d392a5
namphu2911 <118431580+namphu2911@users.noreply.github.com>
2022-11-17 15:51:51 +0700

    Add files via upload

---
+++ MqttDemo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+namespace App1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MqttClient mqttClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mqttClient = new MqttClient("127.0.0.1");
+            mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+            mqttClient.Subscribe(new string[] { "Application1/Message" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            mqttClient.Connect("Application2");
+        }
+
+
+
+        private void MqttClient_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Message);
+            listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add(message)));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                if (mqttClient != null && mqttClient.IsConnected)
+                {
+                    mqttClient.Publish("Application2/Message", Encoding.UTF8.GetBytes(textBox1.Text));
+                }
+            });
+        }
+    }
+}
