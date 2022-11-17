9585a66bdcf54b1bcfd615b7d9619d2e15bd6169
namphu2911 <118431580+namphu2911@users.noreply.github.com>
2022-11-17 15:49:30 +0700

    Add files via upload

---
+++ App1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+
+namespace App1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> MQMessage = new ObservableCollection<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MQResultList.ItemsSource = MQMessage;
+
+            var mqttFactory = new MqttFactory();
+            IMqttClient client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                                          .WithTcpServer("test.mosquitto.org", 8883)
+                                          .WithClientId(Guid.NewGuid().ToString())
+                                          .WithCleanSession(true)
+                                          .Build();
+            client.ConnectAsync(options);
+            
+           
+        }
+
+        public async void PublishBtn_Click(object sender, RoutedEventArgs e, IMqttClient client)
+        {
+            string messagePayload = MessageTb.Text;
+            var message = new MqttApplicationMessageBuilder()
+                                .WithTopic("RishabhSharma")
+                                .WithPayload(messagePayload)
+                                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                                .Build();
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+                
+            }
+
+        }
+
+        
+    }
+}
---
+++ App1/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using App1.Model;
+using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+
+namespace App1.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        //private string _data1;
+        //public string Data1
+        //{
+        //    get => _data1;
+        //    set
+        //    {
+        //        _data1 = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+        //public ObservableCollection<MqttModel> _list;
+
+        //public ObservableCollection<MqttModel> List
+        //{
+        //    get { return _list; }
+        //    set
+        //    {
+        //        _list = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+      
+        
+        //public ICommand PublishCommand { get; set; }
+
+        //public MainViewModel()
+        //{
+        //    var mqttFactory = new MqttFactory();
+        //    var client = mqttFactory.CreateMqttClient();
+        //    var options = new MqttClientOptionsBuilder()
+        //                    .WithClientId(Guid.NewGuid().ToString())
+        //                    .WithTcpServer("test.mosquitto.org", 8883)
+        //                    .WithCleanSession()
+        //                    .Build();
+
+
+
+        //}
+
+       
+
+    }
+}
